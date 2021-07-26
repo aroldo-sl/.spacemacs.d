c85389fa9f8a08066f4974a7c7c4de522ab6c1ae
Aroldo Souza-Leite <asouzaleite@gmx.de>
2021-07-26 15:49:48 +0200

    TODO:user aroldo should work only on the 'aroldo' branch.

--- layers/asl_f8/local/asl_org/asl_org.el
+++ layers/asl_f8/local/asl_org/asl_org.el
@@ -5,16 +5,11 @@
 (require 'org)
 
 ;; ;;; <asl_org-aliases>
-(defalias 'asl_org/store-link                    'org-store-link)
-(defalias 'asl_org/agenda                        'org-agenda)
-(defalias 'asl_org/capture                       'org-capture)
 (defalias 'asl_org/next                          'org-next-visible-heading)
 (defalias 'asl_org/previous                      'org-previous-visible-heading)
 (defalias 'asl_org/forward                       'org-forward-heading-same-level)
 (defalias 'asl_org/backward                      'org-backward-heading-same-level)
-(defalias 'asl_org/outline-up                    'org-outline-up-heading)
 (defalias 'asl_org/jump                          'org-goto)
-(defalias 'asl_org/bullets                       'org-bullets-mode)
 ;; ;;; </asl_org-aliases>
 
 ;; ;;; <asl_org-keys>
@@ -22,17 +17,13 @@
 (let* (
        (key-map org-mode-map)
        (bindings '(
-                   (spacemacs-org-mode-map-prefix [(f8)])
-                   ;; (asl_org/ [(f8)]        )
-                   ;; (asl_org/bullets     [(f8) (-)])
-                   ;; (asl_org/store-link [(f8) (l)]    )
-                   ;; (asl_org/agenda [(f8) (a)]        )
-                   ;; (asl_org/capture [(f8) (c)]       )
-                   ;; (asl_org/jump    [(f8) (j)]       )
-                   ;; (asl_org/next    [(f8) (down)]    )
-                   ;; (asl_org/previous [(f8) (up)]     )
-                   ;; (asl_org/forward [(f8) (next)]    )
-                   ;; (asl_org/backward [(f8) (prior)])
+
+                   (asl_org/ [(f8)]        )
+                   (asl_org/jump    [(f8) (j)]       )
+                   (asl_org/next    [(f8) (down)]    )
+                   (asl_org/previous [(f8) (up)]     )
+                   (asl_org/forward [(f8) (next)]    )
+                   (asl_org/backward [(f8) (prior)])
                    ))
        )
   (loop for (fn key) in bindings do (progn 
