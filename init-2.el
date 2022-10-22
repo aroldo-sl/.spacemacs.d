c2c8ebd9cf14d123c3c2b12c2cb10a4d84fb83de
aroldo <asouzaleite@gmx.de>
2022-10-22 22:08:53 +0200

    Es hat mit dem init-2.el nicht geklappt. Egal, es gibt andere, wichtige Themen zu behandeln.

--- init-2.el
+++
@@ -1,9 +0,0 @@
-;;; init-2.el --- Extenxion of init.el               -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2022  Aroldo Souza-Leite
-
-;; Author: Aroldo Souza-Leite <aroldo@len-LM>
-;; Keywords: docs,
-
-
-(recentf-open-files)
