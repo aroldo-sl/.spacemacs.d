10ad50f1034d9206263cca0fd8ddb63d867ca76a
aroldo <asouzaleite@gmx.de>
2022-10-25 13:39:54 +0200

    sl even better; git-ignoring __pycache__.

--- dig_spacemacs/sl.py
+++ dig_spacemacs/sl.py
@@ -12,41 +12,44 @@ from datetime import datetime
 from pathlib import Path
 __level__ = logging.INFO
 
-class SLogFormatter(logging.Formatter):
-    "A log formatter for SLogHandler"
-    fmt = "%(levelname)s:%(name)s:%(module)s.%(funcName)s:%(lineno)s\n%(message)s"
-    def __init__(self, fmt = None):
-        if fmt is None:
-            fmt = SLogFormatter.fmt
-        super().__init__(fmt = fmt)
+######################### <_get_slog> ################################
+def _get_slog ( level = __level__):
+    class SLogFormatter(logging.Formatter):
+        "A log formatter for SLogHandler"
+        fmt = "%(levelname)s:%(name)s:%(module)s.%(funcName)s:%(lineno)s\n%(message)s"
+        def __init__(self, fmt = None):
+            if fmt is None:
+                fmt = SLogFormatter.fmt
+            super().__init__(fmt = fmt)
 
-class SLogHandler(logging.StreamHandler):
-    "Simple logging handler."
-    def __init__(self, stream = sys.stderr, level = __level__):
-        super().__init__(stream = stream)
-        self.setLevel(level)
-        self.setFormatter(SLogFormatter())
+    class SLogHandler(logging.StreamHandler):
+        "Simple logging handler."
+        def __init__(self, stream = sys.stderr, level = __level__):
+            super().__init__(stream = stream)
+            self.setLevel(level)
+            self.setFormatter(SLogFormatter())
 
-def get_slog(handler = None, level = __level__):
-    "Make a simple logger"
-    if handler is None:
-        handler = SLogHandler()
-    handler.setLevel(level)
-    random_string = "".join(random.sample(string.ascii_letters, 4))
-    log_name  = random_string
-    slog = logging.getLogger(log_name)
-    slog.addHandler(handler)
-    slog.setLevel(level)
-    return slog
-
-slog = get_slog()
+    def get_slog(handler = None, level = __level__):
+        "Make a simple logger"
+        if handler is None:
+            handler = SLogHandler()
+        handler.setLevel(level)
+        random_string = "".join(random.sample(string.ascii_letters, 4))
+        log_name  = random_string
+        slog = logging.getLogger(log_name)
+        slog.addHandler(handler)
+        slog.setLevel(level)
+        return slog
+    return get_slog(level = level)
+############################### </_get_slog>  ###############################################
+_slog = _get_slog(level = __level__)
 
 def _script():
     """
     Runs if this module is called as a
     Python script.
     """
-    slog.info("Perfect!")
+    _slog.info("Perfect!")
 
 if __name__ == "__main__":
    _script()
