19a0f6f53c9d53f06f53d3abc1c3c9df875b7c67
aroldo <asouzaleite@gmx.de>
2022-11-04 20:17:09 +0100

    Wir wollten keine org-meta-return. Stattdessen eigene Funktion asl/org-insert-heading-below definiert und gebunden.

--- layers/asl_f8/local/asl_org/asl_org.el
+++ layers/asl_f8/local/asl_org/asl_org.el
@@ -12,39 +12,25 @@
 (defalias 'asl/org-jump                          'org-goto)
 (defalias 'asl/org-insert-heading-r-c            'org-insert-heading-respect-content)
 ;; ;;; </asl/org aliases>
+;; ;;; <asl/org functions>
+(defun asl/org-insert-heading-below()
+  (interactive)
+  (end-of-line)
+  (org-meta-return))
+;; ;;; </asl/org functions>
 
-;; ;;; <asl/org-keys>
-;; (define-prefix-command 'asl/org)
-;; (let* (
-;;        (key-map org-mode-map)
-;;        (bindings '(
 
-;;                     (asl/org- [(f8)]        )
-;;                    (asl/org-jump    [(f8) (j)]       )
-;;                    (asl/org-next    [(f8) (down)]    )
 
-;;                    (asl/org-previous [(f8) (up)]     )
-;;                    (asl/org-forward [(f8) (next)]    )
-;;                    (asl/org-backward [(f8) (prior)])
-;;                    (asl/org-insert-heading-r-c [(f8) (13)]) ;; 13=RET
-;;                    (asl/org-insert-heading-r-c [(f8) (f8)]) ;; 13=RET
-;;                    ))
-;;        )
-;;   (cl-loop for (fn key) in bindings do (progn (define-key key-map key fn))))
-;;  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;   </als_org/keys>
-
-;; ;;;<asl/org keys>
 ;;;;;;;;;;;;;;;;;;;Using the "Xah syntax" for Emacs key bindings:;;;;;;;
 ;;;;;; http://xahlee.info/emacs/pemacs/keyboard_shortcuts_examples.html ;;;;;;;;
 (define-key org-mode-map (kbd "<f8>")                 nil)
 (define-key org-mode-map (kbd "<f8> <up>")           'asl/org-previous)
 (define-key org-mode-map (kbd "<f8> <next>")         'asl/org-forward)
 (define-key org-mode-map (kbd "<f8> <down>")         'asl/org-next )
-(define-key org-mode-map (kbd "<f8> <up>")           'asl/org-previous)
-(define-key org-mode-map (kbd "<f8> <next>")         'asl/org-forward)
 (define-key org-mode-map (kbd "<f8> <prior>")        'asl/org-backward)
 (define-key org-mode-map (kbd "<f8> <f8>")           'asl/org-insert-heading-r-c ) 
-(define-key org-mode-map (kbd "<f8> RET")            'asl/org-insert-heading-r-c ) 
+(define-key org-mode-map (kbd "<f8> RET")            'asl/org-insert-heading-below) 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;;; </asl/org keys>
 
